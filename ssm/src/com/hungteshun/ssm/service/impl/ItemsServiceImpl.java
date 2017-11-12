4b3afd140baaf73c2a894365ef87a44897e40054
hungteshun <wangyiyx201495@163.com>
2017-11-12 14:08:23 +0800

    ssm工程整合配置

---
+++ ssm/src/com/hungteshun/ssm/service/impl/ItemsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.hungteshun.ssm.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.hungteshun.ssm.mapper.ItemsMapperCustom;
+import com.hungteshun.ssm.po.ItemsCustom;
+import com.hungteshun.ssm.po.ItemsQueryVo;
+import com.hungteshun.ssm.service.ItemsService;
+
+/**
+ * 商品查询service实现类
+ * 
+ * @author hungteshun
+ *
+ */
+public class ItemsServiceImpl implements ItemsService {
+
+	@Autowired
+	private ItemsMapperCustom itemsMapperCustom;
+
+	@Override
+	public List<ItemsCustom> findItemsList(ItemsQueryVo itemsQueryVo) throws Exception {
+		// 通过ItemsMapperCustom查询数据库
+		return itemsMapperCustom.findItemsList(itemsQueryVo);
+	}
+
+}
