8c5bb4e87d212ecaed7961c1a3da913530c53a0b
hungteshun <wangyiyx201495@163.com>
2017-11-18 17:26:26 +0800

    list类型参数绑定

--- ssm/src/com/hungteshun/ssm/po/ItemsQueryVo.java
+++ ssm/src/com/hungteshun/ssm/po/ItemsQueryVo.java
@@ -1,18 +1,25 @@
 package com.hungteshun.ssm.po;
 
+import java.util.List;
+
 /**
  * 商品包装对象
- * 
  * @author hungteshun
  *
  */
 public class ItemsQueryVo {
-
-	// 商品信息
+	
+	//商品信息
 	private Items items;
-
-	// 为了系统 可扩展性，对原始生成的po进行扩展
+	
+	//为了系统 可扩展性，对原始生成的po进行扩展
 	private ItemsCustom itemsCustom;
+	
+	//批量商品信息
+	private List<ItemsCustom> itemsList;
+	
+	//用户信息
+	//private UserCustom userCustom;
 
 	public Items getItems() {
 		return items;
@@ -29,5 +36,15 @@ public class ItemsQueryVo {
 	public void setItemsCustom(ItemsCustom itemsCustom) {
 		this.itemsCustom = itemsCustom;
 	}
+
+	public List<ItemsCustom> getItemsList() {
+		return itemsList;
+	}
+
+	public void setItemsList(List<ItemsCustom> itemsList) {
+		this.itemsList = itemsList;
+	}
+	
+	
 
 }
