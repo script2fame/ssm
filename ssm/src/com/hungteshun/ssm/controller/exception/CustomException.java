83bc568b005825f6ab7ea0c57bcddaa9f1b367b6
hungteshun <wangyiyx201495@163.com>
2017-11-20 13:15:54 +0800

    异常处理

---
+++ ssm/src/com/hungteshun/ssm/controller/exception/CustomException.java
@@ -0,0 +1,29 @@
+package com.hungteshun.ssm.controller.exception;
+
+/**
+ * 系统 自定义异常类，针对预期的异常，需要在程序中抛出此类的异常 
+ * @author hungteshun
+ *
+ */
+
+public class CustomException extends Exception {
+	
+	//异常信息
+	public String message;
+	
+	public CustomException(String message){
+		super(message);
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+
+}
